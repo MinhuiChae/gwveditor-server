9d7db0592fd8881430d2a64a2d05ea9324e54d0f
사용자이름 <>
2023-04-11 16:13:18 +0900

    command 작성

--- src/rest/work.ts
+++ src/rest/work.ts
@@ -11,9 +11,14 @@ import {
 
 import * as dotenv from 'dotenv';
 import WorkService from '../service/workService';
+import runCommand from '../lib/runCommand';
 
 dotenv.config();
 const router = express.Router();
+
+const getExportTxtFile = (filename: string): string => {
+  return `./export/progress-${filename}.txt`
+}
 
 router
   .get("/*", (req: Request, res: Response, next: NextFunction) => {
@@ -28,8 +33,31 @@ router
     let resSend = false;
 
     const spawnCmds = workService.getCommandForSpawn();
-    console.log('spawnCmds > ', spawnCmds)
-    res.send('sucs')
+    const exportFrames = workService.getExportFrames();
+    const exportFileName = workService.getExportFileName();
+    const progressFileName = getExportTxtFile(exportFileName);
+
+    try {
+      runCommand("ffmpeg", spawnCmds, (data:string) => {
+        console.log('data > ', data)
+        
+      }, (args: any) => {
+        console.log('args > ', args)
+        res.send('success')
+      },(err: any) => {
+        console.log("error> ", err);
+        res.status(404).send({
+          status: false,
+          msg: 'FFMPEG Command Error Gernerate Failed',
+          err: err});
+      });
+    } catch(err) {
+      console.log(err)
+    }
+
+
+
+    
   });
 
 export default router;
